337ea678a4a7106a9cf757bcff984f69e65c25a3
Maniol102 <Maniol102@users.noreply.github.com>
2020-10-19 06:44:49 +0200

    Game main function
    Broke it up into smaller functions because main() was too large

--- game.py
+++ game.py
@@ -1,8 +1,7 @@
 from random import randint
-from math import cos
-from math import sin
-
 from player import *
+
+
 def main():
     game_size = [600, 600]
     game_over = False
@@ -16,39 +15,40 @@ def main():
         player2.move()
 
         for i in player1.bullets:
-            if i.position[0] < 0:
+            if offScreen(i, game_size):
                 player1.bullets.remove(i)
-            if i.position[0] > game_size[0]:
+            if intersect(i, player2):
                 player1.bullets.remove(i)
-            if i.position[1] < 0:
-                player1.bullets.remove(i)
-            if i.position[1] > game_size[1]:
-                player1.bullets.remove(i)
-            if (i.position[0] >= player2.position[0] - 0.5 * player2.size):
-                if (i.position[0] <= player2.position[0] + 0.5 * player2.size):
-                    if (i.position[1] >= player2.position[1] - 0.5 * player2.size):
-                        if (i.position[1] <= player2.position[1] + 0.5 * player2.size):
-                            player1.bullets.remove(i)
-                            game_over = True
-                            print("Player1 won")
+                game_over = True
+                print("Player1 won")
         
         for i in player2.bullets:
-            if i.position[0] < 0:
+            if offScreen(i, game_size):
                 player2.bullets.remove(i)
-            if i.position[0] > game_size[0]:
+            if intersect(i, player2):
                 player2.bullets.remove(i)
-            if i.position[1] < 0:
-                player2.bullets.remove(i)
-            if i.position[1] > game_size[1]:
-                player2.bullets.remove(i)
-            if i.position[0] >= player1.position[0] - 0.5 * player1.size:
-                if i.position[0] <= player1.position[0] + 0.5 * player1.size:
-                    if i.position[1] >= player1.position[1] - 0.5 * player1.size:
-                        if i.position[1] <= player1.position[1] + 0.5 * player1.size:
-                            player2.bullets.remove(i)
-                            game_over = True
-                            print("Player2 won")
+                game_over = True
+                print("Player2 won")
 
 
+def offScreen(obj, screensize):
+    if obj.position[0] < 0:
+        return True
+    if obj.position[0] > screensize[0]:
+        return True
+    if obj.position[1] < 0:
+        return True
+    if obj.position[1] > screensize[1]:
+        return True
+    return False
+
+
+def intersect(bullet, player):
+    if bullet.position[0] >= player.position[0]- 0.5 * player.size and \
+    bullet.position[0] <= player.position[0] + 0.5 * player.size and \
+    bullet.position[1] >= player.position[1] - 0.5 * player.size and \
+    bullet.position[1] <= player.position[1] + 0.5 * player.size:
+        return True
+    return False
 
 main()
--- game_graphical.py
+++ game_graphical.py
@@ -15,44 +15,25 @@ def main():
     player1 = Player([randint(0, game_size[0]), randint(0, game_size[1])], (255, 0, 0), 30, game_size)
     player2 = Player([randint(0, game_size[0]), randint(0, game_size[1])], (0, 0, 255), 30, game_size)
 
-
     while not game_over:
         player1.move()
         player2.move()
 
         for i in player1.bullets:
-            if i.position[0] < 0:
+            if offScreen(i, game_size):
                 player1.bullets.remove(i)
-            if i.position[0] > game_size[0]:
+            if intersect(i, player2):
                 player1.bullets.remove(i)
-            if i.position[1] < 0:
-                player1.bullets.remove(i)
-            if i.position[1] > game_size[1]:
-                player1.bullets.remove(i)
-            if (i.position[0] >= player2.position[0] - 0.5 * player2.size):
-                if (i.position[0] <= player2.position[0] + 0.5 * player2.size):
-                    if (i.position[1] >= player2.position[1] - 0.5 * player2.size):
-                        if (i.position[1] <= player2.position[1] + 0.5 * player2.size):
-                            player1.bullets.remove(i)
-                            game_over = True
-                            print("Player1 won")
+                game_over = True
+                print("Player1 won")
         
         for i in player2.bullets:
-            if i.position[0] < 0:
+            if offScreen(i, game_size):
                 player2.bullets.remove(i)
-            if i.position[0] > game_size[0]:
+            if intersect(i, player2):
                 player2.bullets.remove(i)
-            if i.position[1] < 0:
-                player2.bullets.remove(i)
-            if i.position[1] > game_size[1]:
-                player2.bullets.remove(i)
-            if i.position[0] >= player1.position[0] - 0.5 * player1.size:
-                if i.position[0] <= player1.position[0] + 0.5 * player1.size:
-                    if i.position[1] >= player1.position[1] - 0.5 * player1.size:
-                        if i.position[1] <= player1.position[1] + 0.5 * player1.size:
-                            player2.bullets.remove(i)
-                            game_over = True
-                            print("Player2 won")
+                game_over = True
+                print("Player2 won")
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -69,22 +50,40 @@ def main():
             player1.drive()
 
         screen.fill((230, 230, 230))
-
-        pygame.draw.circle(screen, player1.color, (int(player1.position[0]), int(player1.position[1])), player1.size)
-        pygame.draw.circle(screen, (0, 0, 0), (int(player1.position[0] + cos(player1.angle) * 15), int(player1.position[1] + sin(player1.angle) * 15)), 10)
-
-        pygame.draw.circle(screen, player2.color, (int(player2.position[0]), int(player2.position[1])), player2.size)
-        pygame.draw.circle(screen, (0, 0, 0), (int(player2.position[0] + cos(player2.angle) * 15), int(player2.position[1] + sin(player2.angle) * 15)), 10)
-
-        for bullet in player1.bullets:
-            pygame.draw.circle(screen, (0,0,0), (int(bullet.position[0]), int(bullet.position[1])), bullet.size)
-
+        draw_player(player1, screen)
+        draw_player(player2, screen)
         pygame.display.flip()
-        
-        
 
 
     pygame.quit()
+
+def draw_player(player, screen):
+    pygame.draw.circle(screen, player.color, (int(player.position[0]), int(player.position[1])), player.size)
+    pygame.draw.circle(screen, (0, 0, 0), (int(player.position[0] + cos(player.angle) * 15), int(player.position[1] + sin(player.angle) * 15)), 10)
+    for bullet in player.bullets:
+        pygame.draw.circle(screen, (0,0,0), (int(bullet.position[0]), int(bullet.position[1])), bullet.size)
+
+
+def offScreen(obj, screensize):
+    if obj.position[0] < 0:
+        return True
+    if obj.position[0] > screensize[0]:
+        return True
+    if obj.position[1] < 0:
+        return True
+    if obj.position[1] > screensize[1]:
+        return True
+    return False
+
+
+def intersect(bullet, player):
+    if bullet.position[0] >= player.position[0]- 0.5 * player.size and \
+    bullet.position[0] <= player.position[0] + 0.5 * player.size and \
+    bullet.position[1] >= player.position[1] - 0.5 * player.size and \
+    bullet.position[1] <= player.position[1] + 0.5 * player.size:
+        return True
+    return False
+
 
 main()
 
